93d0e8e3ba4c2b58f8684fe035970b824edd313c
masafumi machida <masafumi-machida1@dip-net.co.jp>
2022-11-09 11:25:25 +0900

    minor corrections

--- chapters/chapter3/chapter3_test.go
+++ chapters/chapter3/chapter3_test.go
@@ -6,10 +6,6 @@ import (
 	"testing"
 
 	"github.com/dip-dev/go-test-tutorial/mysql"
-	"github.com/dip-dev/go-test-tutorial/mysql/queries"
-	"github.com/dip-dev/go-test-tutorial/mysql/structs"
-	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
 )
 
 var mysqlCli *mysql.Client
@@ -28,52 +24,3 @@ func TestMain(m *testing.M) {
 }
 
 // FIXME: ↓↓テストを作成する↓↓
-func TestSample(t *testing.T) {
-
-	success := map[string]struct {
-		name string
-		want *structs.MPrefecture
-	}{
-		"正常": {
-			name: "北海道",
-			want: &structs.MPrefecture{
-				ID:       1,
-				Name:     "北海道",
-				Area:     "北海道",
-				LandArea: 83424,
-			},
-		},
-		"0件": {
-			name: "アメリカ",
-			want: nil,
-		},
-	}
-
-	fail := map[string]struct {
-		name string
-	}{
-		"異常（mock）": {
-			name: "北海道",
-		},
-	}
-
-	da := New(mysqlCli, queries.New())
-	for tt, tc := range success {
-		t.Run(tt, func(t *testing.T) {
-			got, _ := da.selectPrefecture(tc.name)
-			assert.Equal(t, tc.want, got)
-		})
-	}
-
-	for tt, tc := range fail {
-		t.Run(tt, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-			mock := queries.NewMockSelecters(ctrl)
-			mock.EXPECT().SelectPrefecture().Return("select * from abcd")
-			d := New(mysqlCli, mock)
-			got, _ := d.selectPrefecture(tc.name)
-			assert.Nil(t, got)
-		})
-	}
-}
--- chapters/chapter4/db/select_test.go
+++ chapters/chapter4/db/select_test.go
@@ -1,16 +1,11 @@
 package db
 
 import (
-	"context"
 	"log"
 	"os"
 	"testing"
 
 	"github.com/dip-dev/go-test-tutorial/mysql"
-	"github.com/dip-dev/go-test-tutorial/mysql/queries"
-	"github.com/dip-dev/go-test-tutorial/mysql/structs"
-	gomock "github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
 )
 
 var mysqlCli *mysql.Client
@@ -26,49 +21,3 @@ func TestMain(m *testing.M) {
 }
 
 // FIXME: ↓↓テストを作成する↓↓
-func TestSample(t *testing.T) {
-	ctx := context.Background()
-
-	success := map[string]struct {
-		area string
-		want []structs.MPrefecture
-	}{
-		"正常": {
-			area: "北海道",
-			want: []structs.MPrefecture{{
-				ID:       1,
-				Name:     "北海道",
-				Area:     "北海道",
-				LandArea: 83424,
-			}},
-		},
-	}
-
-	fail := map[string]struct {
-		area string
-	}{
-		"異常（mock）": {
-			area: "北海道",
-		},
-	}
-
-	da := New(mysqlCli, queries.New())
-	for tt, tc := range success {
-		t.Run(tt, func(t *testing.T) {
-			got, _ := da.SelectPrefectures(ctx, tc.area)
-			assert.Equal(t, tc.want, got)
-		})
-	}
-
-	for tt, tc := range fail {
-		t.Run(tt, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-			mock := queries.NewMockSelecters(ctrl)
-			mock.EXPECT().SelectPrefectures().Return("select * from abcd")
-			d := New(mysqlCli, mock)
-			got, _ := d.SelectPrefectures(ctx, tc.area)
-			assert.Nil(t, got)
-		})
-	}
-}
